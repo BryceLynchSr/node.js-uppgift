51c4daa1c474babd5805fba374e0877cf13c9e4a
BryceLynchSr <dogge84@hotmail.com>
2024-11-19 18:54:59 +0100

    Add files via upload

---
+++ server.js
@@ -0,0 +1,25 @@
+let express = require('express');
+let app = express();
+const path = require('path');
+const fs = require('fs').promises;
+
+app.use(express.static('public'));
+app.use(express.urlencoded({extended: true}));
+
+app.get('/', async function(req, res){
+    res.sendFile(path.join(__dirname, 'contact.html'));
+    })
+
+app.post('/submit', async (req, res) => {
+    await fs.writeFile('data.txt', JSON.stringify(req.body));
+    res.redirect('http://localhost:3000/data');
+})
+
+app.get('/data', async (req, res) => {
+    const data = await fs.readFile('data.txt', 'utf-8');
+    res.send(data);
+})
+
+app.listen(3000, () => {
+    console.log('http://localhost:3000');
+    });
